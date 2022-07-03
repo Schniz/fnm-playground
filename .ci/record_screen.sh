90581ea0c8c81e0cb70cf391baaacd5c96bc4a48
Gal Schlezinger <gal@spitfire.co.il>
2022-07-03 18:30:01 +0300

    wrong location

--- .ci/record_screen.sh
+++ .ci/record_screen.sh
@@ -24,6 +24,6 @@ asciinema rec -c "$DIRECTORY/recorded_screen_script.sh" "$RECORDING_PATH"
 sed "s@$TEMP_DIR@~@g" "$RECORDING_PATH" | \
   svg-term \
     --window \
-    --out "$DIRECTORY/fnm.svg" \
+    --out "docs/fnm.svg" \
     --height=17 \
     --width=70
