e88b35466f55ebfb2c4bb4bc345f148937980b4a
Gal Schlezinger <gal@spitfire.co.il>
2022-07-03 13:38:10 +0300

    wip

--- docs/recorded_screen_script.sh
+++ docs/recorded_screen_script.sh
@@ -6,7 +6,12 @@ GAL_PROMPT_PREFIX='\e[34m✡ \e[0m'
 
 function type() {
   printf $GAL_PROMPT_PREFIX
-  echo $* | pv -qL $[10+(-2 + RANDOM%5)]
+  text_to_write="$*"
+  for (( i=0; i<${#text_to_write}; i++ )); do
+    printf "${text_to_write:$i:1}"
+    sleep 0.0$[(10+(-2+RANDOM%5))/2]
+  done
+  printf "\n"
 }
 
 type 'eval "$(fnm env)"'
